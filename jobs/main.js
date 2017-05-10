6f0834969f6ffc294e7e7136cfbdd23bc42fdc2a
Roy Lara <ifr33m4n@gmail.com>
2017-05-10 16:39:58 -0400

    may 10

--- jobs/main.js
+++ jobs/main.js
@@ -1,16 +1,32 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingScreen from './screens/SettingScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       Welcome: { screen: WelcomeScreen },
       Auth: { screen: AuthScreen },
+      Main: {
+        screen: TabNavigator({
+          Map: { screen: MapScreen },
+          Deck: { screen: DeckScreen },
+          Review: {
+            screen: StackNavigator({
+              Review: { screen: ReviewScreen },
+              Settings: { screen: SettingScreen }
+            })
+          }
+        })
+      },
     });
     return <MainNavigator />;
   }
--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { Button, Card, Icon } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+static navigationOptions = {
+    title: 'Review Jobs',
+    tabBar: {
+      icon: ({ tintColor }) => {
+        return <Icon name="favorite" size={30} color={tintColor} />;
+      }
+    },
+    header: ({ navigate }) => {
+      return {
+        right: (
+          <Button
+            title="Settings"
+            onPress={() => navigate('settings')}
+            backgroundColor="rgba(0,0,0,0)"
+            color="rgba(0, 122, 255, 1)"
+          />
+        ),
+        style: {
+          marginTop: Platform.OS === 'android' ? 24 : 0
+        }
+      };
+    }
+  }
+
     render() {
         return (
             <View>
